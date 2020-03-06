ba8a551f47c43052df3e41cb59289fad69fcaf3f
chamal <ludiantochamal27@gmail.com>
2020-03-06 15:47:11 +0700

    deploy

---
+++ precache-manifest.eb9df4ea2cde50245d88b27241fc86b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2e2d1929d752e3edb0ce7946699a9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b439ec7b5720d894aa85",
+    "url": "/static/css/main.b6e4003a.chunk.css"
+  },
+  {
+    "revision": "a4e36721efd53ee4adf6",
+    "url": "/static/js/2.9d9761fc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.9d9761fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b439ec7b5720d894aa85",
+    "url": "/static/js/main.14f7aaf8.chunk.js"
+  },
+  {
+    "revision": "2fc8e7369f6406e5984d",
+    "url": "/static/js/runtime-main.447fd809.js"
+  }
+]);
